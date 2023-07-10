8d34c51b878ba4fa382087ba66ed3a82d1ad0a15
earnnnnnnnnn <139094586+earnnnnnnnnn@users.noreply.github.com>
2023-07-10 10:39:47 +0700

    Add files via upload

---
+++ earn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout << "student code : 6606021631017"<< endl;
+	cout << "student name : Kochakorn Muengpho"<< endl;
+	cout << "    Division : Information Technoigy"<< endl;
+	cout << "        year : 1"<< endl;
+	cout << "        Room : RB"<< endl;
+	cout << "         Sex : female"<< endl;
+	cout << "         Age : 18"<< endl;
+	system("pause");
+	return(0);
+}
