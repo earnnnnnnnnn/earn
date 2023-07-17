4ece17945ea67bbe1ae7359d1539619f742f8f4b
earn <139094586+earnnnnnnnnn@users.noreply.github.com>
2023-07-17 10:45:25 +0700

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string Name;
+	int Birth;
+	cout << "Entyer name			:";
+	cin >> Name;
+	cout << "Enter Year of birth	:";
+	cin >> Birth;
+	cout << "**********out put**********\n";
+	cout << " You Age =" << 2566 - Birth << "year old" <<endl;
+
+	return(0);
+
+}
