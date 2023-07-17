558840bd0686096f95e37000b97d0c28e343c613
earn <139094586+earnnnnnnnnn@users.noreply.github.com>
2023-07-17 11:48:13 +0700

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	float f;
+	cout <<  "Enter Fahrenheit :";
+	cin >> f;
+	cout << "Celcuis = "<< (5.0/9.0)*(f-32);
+	system("pause");
+
+
+	return (0);
+
+}
