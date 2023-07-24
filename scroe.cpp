abd8474756e18b96a35feb23e84824227ab7ecc1
earn <139094586+earnnnnnnnnn@users.noreply.github.com>
2023-07-24 10:11:28 +0700

    Add files via upload

---
+++ scroe.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float scroe;
+	cout << "Input c++ :";
+	cin >> scroe;
+	cout << "you " << ((scroe>=50)? "pass":"fall") << "\ with score 56;\n";
+
+	system("pause");
+return(0);
+}
