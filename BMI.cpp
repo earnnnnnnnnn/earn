acd1f8f28ebe5b189a8c0395f45c668f0806b28b
earn <139094586+earnnnnnnnnn@users.noreply.github.com>
2023-08-04 15:38:06 +0700

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{	
+	string gender;
+	float age,height,weight,bmi;
+	cout << "Enter age : ";
+	cin >> age;
+	cout << "Enter gender : ";
+	cin >> gender;
+	cout << "Enter height : ";
+	cin >> height;
+	cout << "Enter weight : ";
+	cin >> weight;
+	
+	bmi =  weight / (height/100 * height/100);
+	cout << "BMI = "<< bmi << endl;
+
+	if (bmi < 18.5) cout << "underweight\n";
+		else if (bmi < 24 ) cout << "normal\n";
+			else if (bmi < 29 ) cout << "overweight\n";
+				else if (bmi >= 30 ) cout << "obesity\n";
+
+
+
+
+	system("pause");
+	return(0);
+}
